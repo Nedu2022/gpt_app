3f692908171c07ad2dbf7580c78ee81f3c7b096a
Nedu2022 <nwabuokeichinedu00@gmail.com>
2023-03-03 12:27:05 +0100

    App setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='gradient__bg'>
+        <Navbar></Navbar>
+        <Header></Header>
+      </div>
+      <Brand></Brand>
+      <WhatGPT3></WhatGPT3>
+      <Features></Features>
+      <Possibility></Possibility>
+      <CTA></CTA>
+      <Blog></Blog>
+      <Footer></Footer>
     </div>
   );
 }
