39a794923c2409461c7390064b2d415bb46ec01d
Nedu2022 <nwabuokeichinedu00@gmail.com>
2023-03-05 23:54:03 +0100

    whatGPT3 section

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -3,7 +3,20 @@ import './whatGPT3.css'
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className='gpt__whatgpt3 section__margin' id='whpt3'>
+      <div className="gpt__whatgpt3-feature">
+        {/* <Feature /> */}
+      </div>
+      <div className="gpt__whatgpt3-heading">
+        <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt__whatgpt3-container">
+        {/* <Feature />
+        <Feature />
+        <Feature /> */}
+      </div>
+    </div>
   )
 }
 
